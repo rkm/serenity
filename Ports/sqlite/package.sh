07f325e2c46a7d20d8611cfa1a641affd90f589c
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-04-16 02:19:00 +0100

    Add initial sqlite port with system identifier patch

---
+++ Ports/sqlite/package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash ../.port_include.sh
+
+port=sqlite
+version=3310100
+files="https://sqlite.org/2020/sqlite-autoconf-${version}.tar.gz sqlite-autoconf-${version}.tar.gz"
+workdir="sqlite-autoconf-${version}"
+useconfigure=true
+
+# sha1: 0c30f5b22152a8166aa3bebb0f4bc1f3e9cc508b
+
